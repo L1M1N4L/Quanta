0deba9ad9f43756213cdedabba5eb1059090ff49
L1M1N4L <127649044+L1M1N4L@users.noreply.github.com>
2023-11-27 09:11:32 +0700

    Update interpreter.java

---
+++ Quanta/interpreter.java
@@ -0,0 +1,7 @@
+package com.craftinginterpreters.lox;
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+ public Object visitLiteralExpr(Expr.Literal expr) {
+ return expr.value;
+ }
+}
